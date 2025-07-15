2d4dfba6376ff92744b8db9ca3792b59fe8c9910
AbinashBasnet5 <basnetabinash27@gmail.com>
2025-07-15 22:50:41 +0545

    DB connection

--- Server/src/config/databasae.ts
+++ Server/src/config/databasae.ts
@@ -5,8 +5,27 @@ import { config } from "./environment.config";
       try {
         mongoose.set('strictQuery', false);
        const conn =  await mongoose.connect(config.mongodb.uri, config.mongodb.options);
-      console.log(`MongoDB connected: ${conn.connection.host}`);  
+      console.log(`MongoDB connected: ${conn.connection.host}`);
+       
+       process.on('SIGINT', async () => {
+        await mongoose.connection.close();
+        console.log('Mongo DB connection connection clsoed');
+        process.exit(0);
+         
+       })
     } catch (error) {
+        console.error('MongoDB connection error', error);
+        process.exit(0);
         
       }
- }
+       mongoose.connection.on('error', (error)=> {
+          console.error('MOngoDb connection error:',error);
+       })
+        mongoose.connection.on('disconnected', ()=> {
+              console.log('MongoDb Connection disconnected')
+        });
+        mongoose.connection.on('reconnected', ()=> {
+              console.log('MongoDb reconnected');
+        })
+    }
+ 
