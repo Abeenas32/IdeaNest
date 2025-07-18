11eb3528a5693cf48d14cb97d745617b0800f2bb
AbinashBasnet5 <basnetabinash27@gmail.com>
2025-07-18 14:12:52 +0545

    Authenticator for Request

---
+++ Server/src/middleware/auth.middleware.ts
@@ -0,0 +1,54 @@
+import { AuthenticatedRequest } from './../types/auth.types';
+import { Request, Response, NextFunction } from 'express';
+import { verifyAccessTokens, extractToken } from '../utils/jwt.utils';
+import { sendError } from '../utils/response.utils';
+    
+ export const authenticate = (req: AuthenticatedRequest, res: Response , next :NextFunction) : void => {
+     try {
+        const authHeader = req.headers.authorization;
+        const token = extractToken(authHeader);
+        if(!token){
+            sendError(res, 'Access token is required', 401);
+            return ;
+        }
+        const payload = verifyAccessTokens(token)
+        req.user = payload;
+        next();
+     } catch (error) {
+        sendError(res, 'Invalid or Expired access token', 401);
+    
+     }
+ }
+
+   export const authorize = (roles : string[])  => {
+    return (req : AuthenticatedRequest, res : Response, next : NextFunction): void => {
+        if(!req.user){
+            sendError(res, 'Authentication failed',401);
+            return ;
+        }
+         if(!roles.includes(req.user.role)){
+            sendError(res, 'Invalid Permission', 403);
+            return ; 
+         }
+         next();
+    }
+   }
+
+    // for the guest user cuz they don't have the token 
+    //  required 
+   export const optionalAuth = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = extractToken(authHeader);
+    
+    if (token) {
+      const payload = verifyAccessTokens(token);
+          req.user = payload;
+    }
+    
+    next();
+  } catch (error) {
+    // Continue without authentication for optional auth for guest. 
+    next();
+  }
+};
